b30ba521ea10976c7b6374ac0eaa2cfdc8a1a3a9
DedbaradeD <gavrilkoo@gmail.com>
2019-06-28 02:38:23 +0300

    async strategy #17

--- notifier.cpp
+++ notifier.cpp
@@ -13,26 +13,26 @@ struct myPubMsgInfo {
     char        ID[30];
 };
 
-//static volatile bool done = false;
+static volatile bool done = false;
 // natsOptions* opts   = NULL;
 // const char* cluster    = "cyberway";
 // const char* clientID   = "notifier";
 // const char* subj   = "foo";
 // const char* txt    = "hello";
 
-//static void _publish_ack_cb(const char* guid, const char* error, void* closure) {
+static void _publish_ack_cb(const char* guid, const char* error, void* closure) {
     // TODO: delete object from waiting list, so we can check if some object didn't published for a long time
     //std::cout << "#Ack#, " << guid << std::endl;
     // myPubMsgInfo* pubMsg = (myPubMsgInfo*)closure;
     // printf("Ack handler for message ID=%s Data=%.*s GUID=%s - ", pubMsg->ID, pubMsg->size, pubMsg->payload, guid);
-//    if (error != NULL) {
-//        std::cout << "Error: " << error << std::endl;
-//        done = true;    // TODO: locking
-//    }
+    if (error != NULL) {
+        std::cout << "Error: " << error << std::endl;
+        done = true;    // TODO: locking
+    }
     // free(pubMsg);    // This is a good place to free the pubMsg info since we no longer need it
     // Notify the main thread that we are done. This is not the proper way and you should use some locking.
     // done = true;
-//}
+}
 
 static void sigusr1_handler(int signum) {
     print = !print;
@@ -66,7 +66,7 @@ int main(int argc, char** argv) {
     stanConnOptions_Destroy(connOpts);
 
     std::string line;
-    while (s == NATS_OK) {
+    while (!done && s == NATS_OK) {
         static const auto start = "{\"msg_type\":\"";   // ok, it's ugly. TODO: ?parse json
         static const auto start_len = strlen(start);
         std::getline(std::cin, line);
@@ -102,7 +102,8 @@ int main(int argc, char** argv) {
         // if (s == NATS_OK) {
         // s = stanConnection_PublishAsync(sc, subj, pubMsg->payload, pubMsg->size, _pubAckHandler, (void*)pubMsg);
         for (int i = 0; i < 24 * 1000; ++i) {
-            s = stanConnection_Publish(sc, subj, line.c_str(), line.size());
+            s = stanConnection_PublishAsync(sc, subj, line.c_str(), line.size(), _publish_ack_cb, NULL);
+            //s = stanConnection_Publish(sc, subj, line.c_str(), line.size());
             if (s == NATS_TIMEOUT) {
                 nats_Sleep(50);
                 continue;
